88b69387c607c67ea316d3546149123e0f621809
yeshaopeng@upg.cn <yeshaopeng@upg.cn>
2015-06-18 15:45:35 +0800

    README.md

---
+++ README.md
@@ -0,0 +1,64 @@
+### s2Server
+基于nodejs的轻量级http服务器。
+
+***
+#### 特点
+
+*  通过js文件来配置服务器。
+
+*  扩展了Request对象，可以方便的获取get或者post请求的数据。
+
+***
+#### DEMO
+1. 启动服务器
+ var s2=require("s2server");
+ s2.listen();
+
+2. 配置服务器
+新建一个js文件，内容如下:
+module.exports={
+	"port":3000,
+	"webroot":"webroot",
+	"context":"",
+	"index":"test.html",
+	"unicode":"utf-8",
+	"controller":"./config/router_config",
+	"mine":"./config/mine_config",
+}; 
+port：端口号
+webroot：项目的根路径
+context：项目上下文路径
+index：项目首页
+unicode：编码类型
+controller：控制器配置文件
+mine：mine配置文件
+如果某项不需要自定义，可以省略。
+在启动服务器前调用s2\.loadConfig(modulepath)方法加载配置。
+
+3. controller配置文件
+module.exports= {
+	"common":require("../../test/common")
+}
+
+4. controller
+
+controller在被调用的时候默认会传入两个参数，第一个参数是S2Request类型，第二个参数是Response类型。
+
+	module.exports = {
+		"sayhello": function(request, response) {
+			response.writeHead(200, {
+				'Content-Type': 'text/plain;charset=UTF-8',
+
+			});
+			response.write("hello," + request.queryString("name") + "!");
+			response.end();
+		},
+		"saysorry": function(request, response) {
+			response.writeHead(200, {
+				'Content-Type': 'text/plain;charset=UTF-8',
+			});
+			response.write("sorry,"+request.getParameter("name")+"!");
+			response.end();
+
+		}
+	}
--- test/common.js
+++ test/common.js
@@ -1,60 +1,26 @@
-(function () {
+(function() {
 	// body...
-	var url=require("url");
-	var queryString=require("querystring");
-	var http=require("http");
-	var path=require("path");
-	var util=require("util");
-	var proxy=require("../../proxy").proxy;
-	exports.controller={
-		"sayhello":function(request,response){
+	var url = require("url");
+	var queryString = require("querystring");
+	var http = require("http");
+	var path = require("path");
+	var util = require("util");
+	module.exports = {
+		"sayhello": function(request, response) {
 			response.writeHead(200, {
-         		'Content-Type': 'text/plain;charset=UTF-8',
+				'Content-Type': 'text/plain;charset=UTF-8',
 
-     		});
-    		response.write("hello,"+request.queryString("name")+"!");
-    		response.end();
-		},
-		"saysorry":function(request,response){
-			response.writeHead(200, {
-         			'Content-Type': 'text/plain;charset=UTF-8',
-     			});
-     		// response.write("i miss you,"+request.getParameter("name")+"!");
-     		response.write(JSON.stringify(request.headers)+JSON.stringify(request.data));
-    		response.end();    		
-    		
-		 },
-		"ajaxproxy":function(request,response){
-			var wd=request.queryString("wd");
-			var cont="wd="+wd;
-			cont=encodeURI(cont);
-			var opt="http://www.baidu.com/s?"+cont;
-			// var opt={
-			// 	protocol:"http:",
-			// 	host:"localhost:3001",
-			// 	hostname:"localhost",
-			// 	pathname:"/notes/common/saysorry",
-			// 	//search:"?"+cont,
-			// 	method:"POST",
-			// 	headers:{
-			// 		'Content-Type': 'application/x-www-form-urlencoded', 
-			// 		'Content-Length' : cont.length 
-			// 	}
-			// };
-			proxy.request(opt,cont,function(res){
-				var data="";
-				res.on("data",function(chunk){
-					data+=chunk;
-				});
-				res.on("end",function(chunk){
-					console.log(res.statusCode);
-					response.writeHead(200, {
-         			'Content-Type': 'text/html;charset=UTF-8',
-     				});
-    				response.write(data);
-    				response.end();
-				});
 			});
+			response.write("hello," + request.queryString("name") + "!");
+			response.end();
+		},
+		"saysorry": function(request, response) {
+			response.writeHead(200, {
+				'Content-Type': 'text/plain;charset=UTF-8',
+			});
+			response.write("sorry,"+request.getParameter("name")+"!");
+			response.end();
 
 		}
-	}})()
+	}
+})()
